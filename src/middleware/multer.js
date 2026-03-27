88bffa842a5bcc42c755884cffd5ebc7ae039678
Inna Melnychenko <dovgopolaii@gmail.com>
2026-03-27 19:12:09 +0100

    add logger, time, multer (#12)

---
+++ src/middleware/multer.js
@@ -0,0 +1,34 @@
+import multer from "multer";
+import createHttpError from "http-errors";
+
+const ONE_MB = 1 * 1024 * 1024; // 1MB в байтах
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  const allowedMimeTypes = ["image/jpeg", "image/jpg", "image/png"];
+
+  if (!allowedMimeTypes.includes(file.mimetype)) {
+    return cb(createHttpError(400, "Only JPG and PNG images are allowed"));
+  }
+
+  cb(null, true);
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: ONE_MB },
+  fileFilter,
+});
+
+export const handleMulterError = (err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      return next(createHttpError(400, "Image size must be less than 1MB"));
+    }
+    return next(createHttpError(400, err.message));
+  }
+  next(err);
+};
+
+export const uploadImages = upload.array("images", 10);
